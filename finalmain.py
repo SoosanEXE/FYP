45e3da67d43d94f88abef5c3da2299977826c820
Srikanth S <54700616+srikanth-coder@users.noreply.github.com>
2023-02-20 11:31:14 +0530

    Update finalmain.py

--- finalmain.py
+++ finalmain.py
@@ -5,17 +5,17 @@ import numpy as np
 import warnings
 import pickle
 warnings.filterwarnings('ignore')
-model = joblib.load('AnomalyClassifier/NSLAnoClf.pkl')
-df = pd.read_parquet("DataPreprocess/KDDpreprocessed.parquet")
-df_mul = pd.read_parquet("KDDpreprocessed1.parquet")
+model = joblib.load('FYP/AnomalyClassifier/NSLAnoClf.pkl')
+df = pd.read_parquet("FYP/DataPreprocess/KDDpreprocessed.parquet")
+df_mul = pd.read_parquet("FYP/KDDpreprocessed1.parquet")
 df.drop("attack_class",axis=1,inplace=True)
 predicted_labels =model.predict(df)
 #Convert the predicted_labels to a Pandas DataFrame
 data= pd.DataFrame(predicted_labels, columns=['attack_class'])
 df['attack_class']=data
-model1 = joblib.load('AnomalyClassifier/UNSWAnoClf.pkl')
-df1 = pd.read_parquet("DataPreprocess/UNSWpreprocessed.parquet")
-df1_mul = pd.read_parquet("UNSWpreprocessed1.parquet")
+model1 = joblib.load('FYP/AnomalyClassifier/UNSWAnoClf.pkl')
+df1 = pd.read_parquet("FYP/DataPreprocess/UNSWpreprocessed.parquet")
+df1_mul = pd.read_parquet("FYP/UNSWpreprocessed1.parquet")
 df1.drop("attack_cat",axis=1,inplace=True)
 predicted_labels1 =model1.predict(df1)
 #Convert the predicted_labels to a Pandas DataFrame
@@ -45,7 +45,7 @@ df4=pd.DataFrame(df1.loc[:, col1])
 # loaded_model = joblib.load('AnomalyClassifier/UNSWAnoClf1.pkl')
 # feature_names = loaded_model.feature_names_in_
 # print("Model features:", feature_names)
-model3 = joblib.load('AnomalyClassifier/NSLAnoClf1.pkl')
+model3 = joblib.load('FYP/AnomalyClassifier/NSLAnoClf1.pkl')
 df3.drop("attack_class",axis=1,inplace=True)
 predicted_labels3 =model3.predict(df3)
 #Convert the predicted_labels to a Pandas DataFrame
@@ -53,7 +53,7 @@ data3= pd.DataFrame(predicted_labels3, columns=['attack_class'])
 df3['attack_class']=data3
 print(df3['attack_class'].value_counts())
 print(".................................................................")
-model4 = joblib.load('AnomalyClassifier/UNSWAnoClf1.pkl')
+model4 = joblib.load('FYP/AnomalyClassifier/UNSWAnoClf1.pkl')
 df4.drop("attack_cat",axis=1,inplace=True)
 predicted_labels4 =model4.predict(df4)
 #Convert the predicted_labels to a Pandas DataFrame
